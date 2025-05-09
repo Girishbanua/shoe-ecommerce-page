bacd2e484c529bc0009119d2b5263d532098c8cd
Girish Chandra banua <111453017+Girishbanua@users.noreply.github.com>
2025-05-09 19:40:05 +0530

    responsive design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,106 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { data } from "./data";
-import logo from "./images/logo.svg";
-import { BsCart3 } from "react-icons/bs";
-import avatar from "./images/image-avatar.png";
 
-function Header({visible, setVisible, count, setCount}) {
-  
+function NavbarComp() {
+  const [click, setClick] = useState(false);
+  const [change, setChange] = useState(true);
+
+  const handleNav = () => {
+    setClick(!click);
+    setChange(!change);
+  };
+  return (
+    <>
+      {/* Mobile View */}
+      <div className="sm:hidden z-10 flex items-center justify-center">
+        {change ? (
+          <img
+            src="icons/icon-menu.svg"
+            alt="menu"
+            onClick={handleNav}
+            className="h-5"
+          />
+        ) : (
+          <img
+            src="icons/icon-close.svg"
+            alt="close"
+            className="h-5"
+            onClick={handleNav}
+            loading="lazy"
+          />
+        )}
+      </div>
+      {click && (
+        <div className="sm:hidden absolute -right-5 top-0 p-9 w-[75%] h-screen rounded-l-3xl backdrop-blur-xl z-20 border-l-Grayish-blue/20 border-l-4  ">
+          <ul className="flex flex-col gap-10 my-16 text-xl ">
+            <NavListComp />
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+
+const NavListComp = () => {
+  return (
+    <>
+      <li>Collections</li>
+      <li>Men</li>
+      <li>Women</li>
+      <li>About</li>
+      <li>Contact</li>
+    </>
+  );
+};
+
+function Header({ visible, setVisible, count, setCount }) {
   return (
     <div className="relative">
-      <header className="flex gap-3 border-b border-Grayish-blue p-2 items-center justify-between max-w-6xl mx-auto h-28 relative ">
-        <div className="flex gap-8 justify-start items-center">
+      <header className="flex gap-3 border-b border-Grayish-blue sm:p-2 p-6 items-center justify-between max-w-6xl mx-auto sm:h-28 h-20 relative ">
+        <div className="flex gap-3 sm:gap-8 justify-start items-center">
+          <NavbarComp />
           <img
-            src={logo}
+            src="icons/logo.svg"
             alt="logo"
-            className="hover:scale-110 transition-all ease-in-out duration-300"
+            className="hover:scale-110 smooth-transition"
           />
-          <nav>
+
+          {/*Desktop View */}
+          <nav className="hidden sm:block">
             <ul className="flex gap-8 text-Dark-grayish-blue justify-center items-center text-sm ">
-              <li>Collections</li>
-              <li>Men</li>
-              <li>Women</li>
-              <li>About</li>
-              <li>Contact</li>
+              <NavListComp />
             </ul>
           </nav>
         </div>
 
         <div>
-          <div className="flex justify-center items-center gap-8 relative">
-            {count > 0 && <div className="cartCount bg-Orange text-White rounded-2xl text-[9px] px-2 font-bold absolute top-1 left-3">{count}</div>}
+          <div className="flex justify-center items-center gap-2 sm:gap-8 relative">
+            {count > 0 && (
+              <div className="cartCount z-10 bg-Orange text-White rounded-2xl text-[9px] px-2 font-bold absolute top-1 left-5">
+                {count}
+              </div>
+            )}
             <button
               onClick={() => setVisible(!visible)}
-              className="w-full h-max p-2 scale-150 cursor-pointer hover:scale-[1.75] smooth-transition"
+              className="w-full h-max p-2 cursor-pointer"
             >
-              <BsCart3 />
+              <svg
+                width="22"
+                height="20"
+                className="sm:scale-110"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+                  className="sm:fill-Dark-grayish-blue fill-Black hover:fill-Black smooth-transition "
+                />
+              </svg>
             </button>
             <img
-              src={avatar}
+              src="images/image-avatar.png"
               alt="avatar_image"
-              className="w-12 hover:border-[3px] border-Orange/80 rounded-full"
+              className="w-9 sm:w-10 hover:border-[3px] border-Orange/80 rounded-full"
             />
           </div>
         </div>
@@ -48,33 +110,45 @@ function Header({visible, setVisible, count, setCount}) {
   );
 }
 
-function CartComp({count, setCount}) {
-  
+function CartComp({ count, setCount }) {
   return (
-    <div className="cart absolute bg-White h-[35vh] w-[25vw] top-[12vh] right-0 shadow-xl rounded-lg flex flex-col ">
+    <div className="cart absolute h-[35vh] w-[25vw] top-[12vh] right-0 shadow-2xl rounded-lg flex flex-col bg-White border-Grayish-blue/20 border-2 border-b-4 border-l-4 ">
       <div className="up border-b-Grayish-blue border-b">
         <h2 className="p-3 font-semibold text-Very-dark-blue ">Cart</h2>
       </div>
       <div className="down flex items-center justify-center h-full p-2 ">
-        {
-          count > 0 ? <ProductComp count={count} setCount={setCount} /> : <p className="text-Grayish-blue font-semibold ">Your cart is empty.</p>
-        }
+        {count > 0 ? (
+          <ProductComp count={count} setCount={setCount} />
+        ) : (
+          <p className="text-Grayish-blue font-semibold ">
+            Your cart is empty.
+          </p>
+        )}
       </div>
     </div>
   );
 }
 
-function ProductComp({count, setCount}) {
+function ProductComp({ count, setCount }) {
   return (
     <div className="product ">
       <div className="flex gap-6 justify-between items-center">
-        <img src="/images/image-product-1-thumbnail.jpg" alt="Shoes" className="h-16 rounded-lg" />
+        <img
+          src="images/image-product-1-thumbnail.jpg"
+          alt="Shoes"
+          className="h-16 rounded-lg"
+        />
         <div>
-          <p className="text-Dark-grayish-blue" >Fall Limited Edition Sneakers</p>
-          <p className="text-Dark-grayish-blue" >$125.00 <span> x {count}</span> <b className="text-Very-dark-blue">${count * 125}.00</b></p> 
+          <p className="text-Dark-grayish-blue">
+            Fall Limited Edition Sneakers
+          </p>
+          <p className="text-Dark-grayish-blue">
+            $125.00 <span> x {count}</span>{" "}
+            <b className="text-Very-dark-blue">${count * 125}.00</b>
+          </p>
         </div>
         <button onClick={() => setCount(0)}>
-          <img src="/images/delete.png" alt="delete" className="h-7 " />
+          <img src="images/delete.png" alt="delete" className="h-7 " />
         </button>
       </div>
       <button className="orange-button mt-5">Checkout</button>
@@ -88,23 +162,28 @@ export default function App() {
   const [enable, setEnable] = useState(true);
   const [visible, setVisible] = useState(false);
 
-  useEffect(()=>{
-    count > 0 ? setEnable(false) : setEnable(true)
-  },[count])
+  useEffect(() => {
+    count > 0 ? setEnable(false) : setEnable(true);
+  }, [count]);
 
   return (
     <>
-      <Header visible={visible} setVisible={setVisible} count={count} setCount={setCount}/>
-      <section className="p-10 mt-4 flex justify-evenly w-full gap-8">
-        <div className="heroLeft w-max flex flex-col gap-5 p-4">
-          <div className="w-max">
+      <Header
+        visible={visible}
+        setVisible={setVisible}
+        count={count}
+        setCount={setCount}
+      />
+      <section className="sm:p-10 sm:mt-4 flex flex-col sm:flex-row justify-evenly w-full sm:gap-8 gap-6">
+        <div className="heroLeft sm:w-max sm:flex flex-col gap-5 sm:p-4">
+          <div className="sm:w-max h-[18rem] w-full sm:h-auto">
             <img
-              src="/images/image-product-1.jpg"
+              src="images/image-product-1.jpg"
               alt="hero-image"
-              className="rounded-2xl h-[24rem]"
+              className="sm:rounded-2xl sm:h-[24rem] object-cover h-full w-full "
             />
           </div>
-          <div className="flex justify-between">
+          <div className="hidden sm:flex justify-between">
             {products.map((item) => (
               <div key={item.id} className="rounded-2xl">
                 <img
@@ -117,43 +196,60 @@ export default function App() {
           </div>
         </div>
 
-        <div className="heroRight w-[50%] p-8 self-center">
-          <h2 className="uppercase mb-4 font-semibold tracking-widest text-Orange ">
+        <div className="heroRight sm:w-[50%] sm:p-8 px-6 self-center">
+          <h2 className="text-sm uppercase sm:mb-4 mb-2 font-semibold tracking-widest text-Orange ">
             sneaker company
           </h2>
-          <h1 className="text-5xl mb-8 font-bold text-Very-dark-blue ">
+          <h1 className="text-3xl sm:text-5xl mb-4 sm:mb-8 font-bold text-Very-dark-blue ">
             Fall Limited Edition Sneakers
           </h1>
-          <p className="text-Dark-grayish-blue mb-4 leading-relaxed ">
+          <p className="text-Dark-grayish-blue mb-4 leading-relaxed text-[0.95rem] sm:text-lg ">
             These low-profile sneakers are your perfect casual wear companion.
             Featuring a durable rubber outer sole, they’ll withstand everything
             the weather can offer.
           </p>
-          <div className="pricetag flex items-center">
-            <h2 className="text-3xl font-bold text-Very-dark-blue">$125.00</h2>
-            <span className="text-Orange bg-Pale-orange text-lg font-bold rounded-md px-3 py-1 ml-4 ">
-              50%
-            </span>
+          <div className="pricetag flex sm:flex-col sm:items-start items-center justify-between">
+            <div className="flex items-center">
+              <h2 className="text-2xl sm:text-3xl font-bold text-Very-dark-blue">
+                $125.00
+              </h2>
+              <span className="text-Orange bg-Pale-orange text-lg font-bold rounded-md px-3 py-1 ml-4 ">
+                50%
+              </span>
+            </div>
+            <p className="text-Grayish-blue font-bold line-through ">
+              $250.00 0
+            </p>
           </div>
-          <p className="text-Grayish-blue font-bold line-through ">$250.00 0</p>
-          <div className="actionBtn flex mt-8 gap-4">
-            <div className="counter flex justify-around items-center w-[50%]  bg-Light-grayish-blue rounded-xl">
+
+          <div className="actionBtn flex flex-col sm:flex-row mt-8 gap-4">
+            <div className="counter sm:p-0 p-2 flex justify-around items-center sm:w-[50%] bg-Light-grayish-blue rounded-xl">
               <button
-                className="text-Orange text-bold text-3xl"
+                className="text-Orange text-bold text-4xl"
                 onClick={() => count > 0 && setCount(count - 1)}
               >
                 -
               </button>
               <p className="font-semibold">{count}</p>
-              <button              
-                className="text-Orange text-bold text-3xl "
+              <button
+                className="text-Orange text-bold text-4xl "
                 onClick={() => setCount(count + 1)}
               >
                 +
               </button>
             </div>
-            <button onClick={() => setVisible(true)} disabled={enable} className=" orange-button ">
-              <BsCart3 /> Add to cart
+            <button
+              onClick={() => setVisible(true)}
+              disabled={enable}
+              className=" orange-button "
+            >
+              <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+                  fill="#ffffff"
+                />
+              </svg>
+              Add to cart
             </button>
           </div>
         </div>
